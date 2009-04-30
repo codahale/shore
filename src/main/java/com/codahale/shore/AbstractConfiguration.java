32ab18642a76975e85f6adbe7646e314eabfa0a4
Coda Hale <coda.hale@gmail.com>
2009-04-30 01:46:56 -0700

    Added AbstractConfiguration.

---
+++ src/main/java/com/codahale/shore/AbstractConfiguration.java
@@ -0,0 +1,170 @@
+package com.codahale.shore;
+
+import static com.google.common.base.Preconditions.*;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.Entity;
+import javax.servlet.Filter;
+import javax.ws.rs.Path;
+import javax.ws.rs.ext.Provider;
+
+import org.mortbay.jetty.Connector;
+import org.mortbay.jetty.bio.SocketConnector;
+import org.mortbay.jetty.servlet.FilterHolder;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.inject.Module;
+import com.google.inject.Stage;
+
+/**
+ * A base class for configuring Shore applications. Subclass it to create the
+ * configuration for your app:
+ * <pre>
+ * import com.codahale.shore.AbstractConfiguration;
+ * 
+ * public class MyConfiguration extends AbstractConfiguration {
+ * 		@Override
+ * 		protected void configure() {
+ * 			addEntityPackage(Package.getInstance("com.example.myapp.entities"));
+ * 			addResourcePackage(Package.getInstance("com.example.myapp.resources"));
+ * 			addResourcePackage(Package.getInstance("com.example.myapp.providers"));
+ * 			addModule(new DAOModule());
+ * 			setStage(Stage.PRODUCTION);
+ * 			addServletFilter(GzipFilter.class, "/*");
+ * `	}
+ * }
+ * </pre>
+ * 
+ * @author coda
+ *
+ */
+public abstract class AbstractConfiguration {
+	private final List<Package> entityPackages = Lists.newLinkedList();
+	private final List<Package> resourcePackages = Lists.newLinkedList();
+	private final List<Module> modules = Lists.newLinkedList();
+	private final Map<FilterHolder, String> servletFilters = Maps.newLinkedHashMap();
+	private Stage stage = Stage.DEVELOPMENT;
+	
+	/**
+	 * Add a package containing Hibernate {@link Entity} classes.
+	 * 
+	 * @param entityPackage a package containing Hibernate entity classes
+	 */
+	protected void addEntityPackage(Package entityPackage) {
+		checkNotNull(entityPackage);
+		entityPackages.add(entityPackage);
+	}
+	
+	/**
+	 * Add a Guice module.
+	 * 
+	 * @param module a Guice module
+	 */
+	protected void addModule(Module module) {
+		checkNotNull(module);
+		modules.add(module);
+	};
+	
+	/**
+	 * Add a package containing Jersey {@link Provider} classes or
+	 * {@link Path}-annotated resource classes.
+	 * 
+	 * @param resourcePackage a package containing Jersey classes
+	 */
+	protected void addResourcePackage(Package resourcePackage) {
+		checkNotNull(resourcePackage);
+		resourcePackages.add(resourcePackage);
+	}
+	
+	/**
+	 * Add a servlet {@link Filter}.
+	 * 
+	 * @param filterClass the filter's class
+	 * @param urlPattern the URL pattern for requests to filter
+	 */
+	protected void addServletFilter(Class<? extends Filter> filterClass, String urlPattern) {
+		checkNotNull(filterClass);
+		checkNotNull(urlPattern);
+		final FilterHolder holder = new FilterHolder(filterClass);
+		servletFilters.put(holder, urlPattern);
+	};
+	
+	/**
+	 * Add a servlet {@link Filter} in a Jetty {@link FilterHolder}.
+	 * 
+	 * @param holder a configred filter holder
+	 * @param urlPattern the URL pattern for requests to filter
+	 */
+	protected void addServletFilter(FilterHolder holder, String urlPattern) {
+		checkNotNull(holder);
+		checkNotNull(urlPattern);
+		servletFilters.put(holder, urlPattern);
+	}
+	
+	/**
+	 * Configures this configuration via the exposed protected methods.
+	 */
+	protected abstract void configure();
+	
+	/**
+	 * Returns a new {@link Connector} to be used by Jetty to response to HTTP
+	 * reqests. Override this method to return a specifically-configured
+	 * {@link Connector}. By default, a stock {@link SocketConnector} is
+	 * returned.
+	 * 
+	 * @return a {@link SocketConnector}
+	 */
+	public Connector getConnector() {
+		return new SocketConnector();
+	}
+	
+	/**
+	 * Returns a list of Hibernate entity packages.
+	 */
+	public List<Package> getEntityPackages() {
+		return ImmutableList.copyOf(entityPackages);
+	}
+	
+	/**
+	 * Returns a list of Guice modules.
+	 */
+	public List<Module> getModules() {
+		return ImmutableList.copyOf(modules);
+	}
+	
+	/**
+	 * Returns a list of Jersey resource/provider packages.
+	 */
+	public List<Package> getResourcePackages() {
+		return ImmutableList.copyOf(resourcePackages);
+	}
+	
+	/**
+	 * Returns an ordered map of servlet filter holders and their URL patterns.
+	 */
+	public Map<FilterHolder, String> getServletFilters() {
+		return ImmutableMap.copyOf(servletFilters);
+	}
+	
+	/**
+	 * Returns the Guice stage.
+	 */
+	public Stage getStage() {
+		return stage;
+	}
+	
+	/**
+	 * Sets the {@link Stage} that Guice uses when binding classes.
+	 * 
+	 * @param stage Guice's stage
+	 */
+	protected void setStage(Stage stage) {
+		checkNotNull(stage);
+		this.stage = stage;
+	}
+}
---
+++ src/test/java/com/codahale/shore/test/AbstractConfigurationTest.java
@@ -0,0 +1,113 @@
+package com.codahale.shore.test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import static org.junit.matchers.JUnitMatchers.*;
+
+import java.util.List;
+import java.util.Map.Entry;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.mortbay.jetty.bio.SocketConnector;
+import org.mortbay.jetty.servlet.FilterHolder;
+import org.mortbay.servlet.GzipFilter;
+import org.mortbay.servlet.ThrottlingFilter;
+import org.mortbay.servlet.WelcomeFilter;
+
+import com.codahale.shore.AbstractConfiguration;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import com.google.inject.AbstractModule;
+import com.google.inject.Module;
+import com.google.inject.Stage;
+
+@RunWith(Enclosed.class)
+public class AbstractConfigurationTest {
+	private static class MockModule extends AbstractModule {
+		@Override
+		protected void configure() {
+			
+		}
+	}
+	private static Module MOCK_MODULE = new MockModule();
+	
+	private static class MockConfiguration extends AbstractConfiguration {
+		@Override
+		protected void configure() {
+			addEntityPackage(Package.getPackage("com.codahale.shore.test"));
+			addResourcePackage(Package.getPackage("com.codahale.shore.test"));
+			addModule(MOCK_MODULE);
+			setStage(Stage.PRODUCTION);
+			addServletFilter(GzipFilter.class, "/*");
+			addServletFilter(ThrottlingFilter.class, "/hard-to-serve/*");
+			addServletFilter(new FilterHolder(WelcomeFilter.class), "/welcome");
+		}
+		
+		public void doConfig() {
+			configure();
+		}
+	}
+	
+	public static class A_Configuration {
+		private MockConfiguration config;
+		
+		@Before
+		public void setup() throws Exception {
+			this.config = new MockConfiguration();
+			config.doConfig();
+		}
+		
+		@Test
+		public void itHasAListOfEntityPackages() throws Exception {
+			assertThat(
+				config.getEntityPackages(),
+				hasItem(Package.getPackage("com.codahale.shore.test"))
+			);
+		}
+		
+		@Test
+		public void itHasAListOfResourcePackages() throws Exception {
+			assertThat(
+				config.getResourcePackages(),
+				hasItem(Package.getPackage("com.codahale.shore.test"))
+			);
+		}
+		
+		@Test
+		public void itHasGuiceModules() throws Exception {
+			assertThat(
+				config.getModules(),
+				hasItem(MOCK_MODULE)
+			);
+		}
+		
+		@Test
+		public void itHasAGuiceStage() throws Exception {
+			assertThat(config.getStage(), is(Stage.PRODUCTION));
+		}
+		
+		@Test
+		public void itHasAConnector() throws Exception {
+			assertThat(config.getConnector(), is(SocketConnector.class));
+		}
+		
+		@Test
+		public void itHasServletFiltersInOrderOfAddition() throws Exception {
+			final List<String> filters = Lists.newLinkedList();
+			final List<String> patterns = Lists.newLinkedList();
+			for (Entry<FilterHolder, String> entry : config.getServletFilters().entrySet()) {
+				filters.add(entry.getKey().getHeldClass().getName());
+				patterns.add(entry.getValue());
+			}
+			
+			final List<String> expectedFilterClasses = ImmutableList.of("org.mortbay.servlet.GzipFilter", "org.mortbay.servlet.ThrottlingFilter", "org.mortbay.servlet.WelcomeFilter");
+			assertThat(filters, is(expectedFilterClasses));
+			
+			final List<String> expectedUrlPatterns = ImmutableList.of("/*", "/hard-to-serve/*", "/welcome");
+			assertThat(patterns, is(expectedUrlPatterns));
+		}
+	}
+}
