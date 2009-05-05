351ca99444a7b7702e4f8ee4e3a5e895c6017f68
Coda Hale <coda.hale@gmail.com>
2009-05-05 01:27:58 -0700

    Added SchemaCommand.

---
+++ src/main/java/com/codahale/shore/SchemaCommand.java
@@ -0,0 +1,108 @@
+package com.codahale.shore;
+
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Settings;
+import org.hibernate.tool.hbm2ddl.DatabaseMetadata;
+
+import com.codahale.shore.modules.HibernateModule;
+
+
+/**
+ * Outputs a schema or migration SQL script for a Shore application.
+ * 
+ * @author coda
+ *
+ */
+public class SchemaCommand implements Runnable {
+	private final AbstractConfiguration configuration;
+	private final Properties properties;
+	private final boolean migration;
+	private final OutputStream output;
+	
+	/**
+	 * Creates a new {@link SchemaCommand}.
+	 * 
+	 * @param configuration
+	 *            the application's configuration
+	 * @param properties
+	 *            the connection properties
+	 * @param migration
+	 *            if {@code true}, generate a migration script
+	 * @param output
+	 *            the stream to write the script to
+	 */
+	public SchemaCommand(AbstractConfiguration configuration, Properties properties, boolean migration, OutputStream output) {
+		this.configuration = configuration;
+		this.properties = properties;
+		this.migration = migration;
+		this.output = output;
+	}
+	
+	public AbstractConfiguration getConfiguration() {
+		return configuration;
+	}
+	
+	public Properties getProperties() {
+		return properties;
+	}
+	
+	public boolean isMigration() {
+		return migration;
+	}
+	
+	public OutputStream getOutputStream() {
+		return output;
+	}
+	
+	@Override
+	public void run() {
+		final PrintWriter writer = new PrintWriter(output);
+		
+		try {
+			final Logger silentLogger = Logger.getLogger(SchemaCommand.class.getCanonicalName());
+			silentLogger.setLevel(Level.OFF);
+			
+			final HibernateModule module = new HibernateModule(silentLogger, properties, configuration.getEntityPackages());
+			final AnnotationConfiguration hibernateConfig = module.getConfiguration();
+			final Settings settings = hibernateConfig.buildSettings();
+			
+			
+			
+			if (migration) {
+				final Connection connection = settings.getConnectionProvider().getConnection();
+				try {
+					final DatabaseMetadata metadata = new DatabaseMetadata(connection, settings.getDialect());
+					
+					writer.println("/* migration script */");
+					printSQL(writer, hibernateConfig.generateSchemaUpdateScript(settings.getDialect(), metadata));
+				} finally {
+					connection.close();
+				}
+			} else {
+				writer.println("/* full drop-and-create script */");
+				printSQL(writer, hibernateConfig.generateDropSchemaScript(settings.getDialect()));
+				printSQL(writer, hibernateConfig.generateSchemaCreationScript(settings.getDialect()));
+			}
+		} catch (Exception e) {
+			writer.println("Error: unable to connect to the database.\n");
+			e.printStackTrace(writer);
+		}
+		
+		writer.flush();
+	}
+
+	private void printSQL(PrintWriter writer, String[] sqlCmds) {
+		for (String cmd : sqlCmds) {
+			writer.print(cmd);
+			writer.println(";");
+		}
+	}
+
+}
