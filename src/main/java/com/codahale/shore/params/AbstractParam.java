ee3cccfcf0596a6ba4a7350a9c82bd2fe0bd28c2
Coda Hale <coda.hale@gmail.com>
2009-05-05 20:34:30 -0700

    Added AbstractParam.

---
+++ src/main/java/com/codahale/shore/params/AbstractParam.java
@@ -0,0 +1,85 @@
+package com.codahale.shore.params;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+/**
+ * An abstract base class for parameter classes.
+ * 
+ * @author coda
+ *
+ * @param <V> the type of value returned by descendent classes
+ */
+public abstract class AbstractParam<V> {
+	private final V value;
+	
+	/**
+	 * Parses {@code param}, throwing an exception if {@code param} is
+	 * unparsable.
+	 * 
+	 * @param param an external input from a path, query, form, or other parameter
+	 * @throws WebApplicationException if {@code param} is unparsable
+	 */
+	public AbstractParam(String param) throws WebApplicationException {
+		try {
+			this.value = parse(param);
+		} catch (Throwable e) {
+			throw new WebApplicationException(onError(param, e));
+		}
+	}
+	
+	/**
+	 * Returns the parsed value.
+	 * 
+	 * @return the parsed value
+	 */
+	public V getValue() {
+		return value;
+	}
+	
+	@Override
+	public String toString() {
+		return value.toString();
+	}
+	
+	/**
+	 * Parses {@code param} and returns it as an instance of type {@code V}.
+	 * 
+	 * @param param an external input from a path, query, form, or other parameter
+	 * @return {@code param} as an instance of type {@code V}
+	 * @throws Throwable if {@code param} is unparsable
+	 */
+	protected abstract V parse(String param) throws Throwable;
+	
+	/**
+	 * Returns a 400 Bad Request response describing the invalid parameter.
+	 *
+	 * @param param the invalid parameter
+	 * @param e the type of exception thrown by {@link #parse(String)}
+	 * @return a {@link Response} describing the problem parsing {@code param}
+	 */
+	protected Response onError(String param, Throwable e) {
+		return Response
+				.status(Status.BAD_REQUEST)
+				.entity(getErrorMessage(param, e))
+				.build();
+	}
+
+	/**
+	 * Returns an error message describing the invalid parameter.
+	 * 
+	 * @param param the invalid parameter
+	 * @param e the type of exception thrown by {@link #parse(String)}
+	 * @return a string describing the problem parsing {@code param}
+	 */
+	protected String getErrorMessage(String param, Throwable e) {
+		return new StringBuilder()
+					.append("Invalid parameter: ")
+					.append(param)
+					.append(" (")
+					.append(e.getMessage())
+					.append(").")
+					.toString();
+	}
+}
---
+++ src/test/java/com/codahale/shore/params/test/AbstractParamTest.java
@@ -0,0 +1,50 @@
+package com.codahale.shore.params.test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+import com.codahale.shore.params.AbstractParam;
+
+@RunWith(Enclosed.class)
+public class AbstractParamTest {
+	private static class IntegerParam extends AbstractParam<Integer> {
+
+		public IntegerParam(String param) throws WebApplicationException {
+			super(param);
+		}
+
+		@Override
+		protected Integer parse(String param) throws Throwable {
+			return Integer.valueOf(param);
+		}
+	}
+	
+	public static class An_AbstractParam_Descendant {
+		@Test
+		public void itParsesAStringParameterAndReturnsAParamWithAValue() throws Exception {
+			final IntegerParam param = new IntegerParam("200");
+			
+			assertThat(param.getValue(), is(200));
+			assertThat(param.toString(), is("200"));
+		}
+		
+		@Test
+		public void itThrowsAWebApplicationExceptionForUnparsableValues() throws Exception {
+			try {
+				new IntegerParam("200$$00");
+				fail("should have thrown a WebApplicationException but didn't");
+			} catch (WebApplicationException e) {
+				final Response response = e.getResponse();
+				assertThat(response.getStatus(), is(400));
+				assertThat((String) response.getEntity(), is("Invalid parameter: 200$$00 (For input string: \"200$$00\")."));
+			}
+		}
+	}
+}
