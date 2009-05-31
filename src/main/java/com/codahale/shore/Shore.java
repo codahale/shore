90e933f5a2908c086245342c6419fd529bd38249
Coda Hale <coda.hale@gmail.com>
2009-05-30 19:09:25 -0700

    Make Shore final instead of abstract.

--- src/main/java/com/codahale/shore/Shore.java
+++ src/main/java/com/codahale/shore/Shore.java
@@ -2,7 +2,7 @@ package com.codahale.shore;
 
 import static com.google.common.base.Preconditions.*;
 
-public abstract class Shore {
+public final class Shore {
 	private Shore() {} // nope
 	
 	public static void run(AbstractConfiguration configuration, String... arguments)
